6cc51eba6adc62d7ea3653c1df49f95ee5537144
Jakob <u1092049@utah.edu>
2020-01-22 22:59:44 -0700

    wip

--- ceiling.cpp
+++ ceiling.cpp
@@ -3,8 +3,30 @@
 #include <string>
 #include <vector>
 
-int countDifferentPrototypes(std::vector<std::vector<int>> &prototypes) {
+class Tree {
+  
+public:
+  int value;
+  Node* left;
+  Node* right;
+  
+  Node(int _value) {
+    value = _value;
+    Node* right = NULL;
+    Node* left = NULL;
+  }
+};
 
+
+int countDifferentPrototypes(std::vector<std::vector<int>> &prototypes, int numProto, int numLayers) {
+  for (int i = 0; i < numLayers; i++) {
+    for (int j = 0; j < numProto; j++) {
+      if (j == 0) {
+	Node  
+      }
+    }
+  }
+  
   return 0;
 }
 
@@ -35,5 +57,5 @@ int main (int argc, char *argv[]) {
     pCount++;
   }
 
-  std::cout << countDifferentPrototypes(prototypes) << std::endl;
+  std::cout << countDifferentPrototypes(prototypes, numProto, numLayers) << std::endl;
 }
