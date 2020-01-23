4cd78cb3670a9b2a60937fa21fe76c864acad4ae
Jakob <jakob_horvath@hotmail.com>
2020-01-23 13:48:10 -0700

    completed tree creation, needs comparison alg now

--- ceiling.cpp
+++ ceiling.cpp
@@ -4,25 +4,62 @@
 #include <vector>
 
 class Tree {
-  
+
+  class Node {
+    
+  public:
+    int value;
+    Node* left;
+    Node* right;
+    
+    Node(int _value) {
+      value = _value;
+      right = NULL;
+      left = NULL;
+    }
+  };
+
+  Node *root;
+
+  void insert(Node *current, int data) {
+    if (data < current->value) {
+      if (current->left != NULL) {
+	insert(current->left, data);
+      }
+      else {
+	current->left = new Node(data);
+      }
+    }
+    else {
+      if (current->right != NULL) {
+	insert(current->right, data);
+      }
+      else {
+	current->right = new Node(data);
+      }
+    }
+  }
+
 public:
-  int value;
-  Node* left;
-  Node* right;
-  
-  Node(int _value) {
-    value = _value;
-    Node* right = NULL;
-    Node* left = NULL;
+  Tree(int data) {
+    root = new Node(data);
+  }
+
+  void insertNewNode(int data) {
+    insert(root, data);
   }
 };
 
 
 int countDifferentPrototypes(std::vector<std::vector<int>> &prototypes, int numProto, int numLayers) {
-  for (int i = 0; i < numLayers; i++) {
-    for (int j = 0; j < numProto; j++) {
-      if (j == 0) {
-	Node  
+  std::vector<Tree> trees; 
+  for (int p = 0; p < numProto; p++) {
+    for (int l = 0; l < numLayers; l++) {
+      if (l == 0) {
+	trees.push_back(Tree(prototypes[p].at(l)));
+      }
+      else {
+	trees.at(p).insertNewNode(prototypes[p].at(l));
       }
     }
   }
