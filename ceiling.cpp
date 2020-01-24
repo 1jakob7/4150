fef281a92bbfb3a46409af1277fe951709cf1881
Jakob <u1092049@utah.edu>
2020-01-23 22:00:58 -0700

    functioning as expected

--- ceiling.cpp
+++ ceiling.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 
+/// Handles BST creation, as well as comparisons between two trees.
 class Tree {
 
   class Node {
@@ -40,6 +41,19 @@ class Tree {
     }
   }
 
+  bool isSame(Node* aCurrent, Node* bCurrent) {
+    if (aCurrent != NULL && bCurrent != NULL) {
+      return (isSame(aCurrent->right, bCurrent->right)
+	      && isSame(aCurrent->left, bCurrent->left));
+    }
+    else if (aCurrent == NULL && bCurrent == NULL) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
 public:
   Tree(int data) {
     root = new Node(data);
@@ -48,10 +62,20 @@ public:
   void insertNewNode(int data) {
     insert(root, data);
   }
+
+  bool isSameStructure(Tree other) {
+    return isSame(root, other.root);
+  }
 };
 
-
+/// Tests a list of trees for structual differences (how they would be constructed visually)
+/// and returns the number of different "designs".
 int countDifferentPrototypes(std::vector<std::vector<int>> &prototypes, int numProto, int numLayers) {
+  if (prototypes.size() == 1) {
+    return 1;
+  }
+
+  // take the input data and construct a list of trees
   std::vector<Tree> trees; 
   for (int p = 0; p < numProto; p++) {
     for (int l = 0; l < numLayers; l++) {
@@ -64,7 +88,29 @@ int countDifferentPrototypes(std::vector<std::vector<int>> &prototypes, int numP
     }
   }
   
-  return 0;
+  int numDiff = 0;
+  bool hasMatch[numProto];
+  for(bool val : hasMatch) {
+    val = false;
+  }
+  // tests trees against each other, and skips trees that are already matched with a previous
+  for (int p = 0; p < numProto - 1; p++) {
+    if (hasMatch[p] == false) {
+      for (int i = p + 1; i < numProto; i++) {
+        if (hasMatch[i] == false && trees[p].isSameStructure(trees[i])) {
+	  hasMatch[p] = true;
+	  hasMatch[i] = true;
+        }
+      }
+      numDiff++;
+    }
+  }
+  // check the final prototype since the loop skips it's final contribution
+  if (hasMatch[numProto-1] == false) {
+    numDiff++;
+  }
+  
+  return numDiff;
 }
 
 int main (int argc, char *argv[]) {
@@ -80,6 +126,7 @@ int main (int argc, char *argv[]) {
   int val;
   int pCount = 0;
   std::vector<std::vector<int>> prototypes;
+  // take in the specified number of prototypes
   while (pCount < numProto) {
     int lCount = 0;
     std::getline(std::cin, data);
