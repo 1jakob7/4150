4b996bb61b4ade08c416e93c77ca42aaa4e77202
Jakob <u1092049@utah.edu>
2020-01-31 00:02:52 -0700

    running into memory issues

--- galaxy.cpp
+++ galaxy.cpp
@@ -1,10 +1,93 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <cmath>
 
-std::string findMajorGalaxy(std::vector<long> xCords, std::vector<long> yCords) {
+std::vector<long> findMajority(std::vector<long> &xCords, std::vector<long> &yCords, long lo, long hi, long d) {
+  if (lo >= hi) {
+    return {-1, -1};
+  }
+  else if (hi - lo == 1) {
+    return {xCords.at(hi), yCords.at(hi)};
+  }
+  else {
+    long xCount = 0;
+    long yCount = 0;
+    std::vector<long> a = findMajority(xCords, yCords, lo, hi/2, d); // -1 +1?
+    std::vector<long> b = findMajority(xCords, yCords, hi/2, hi, d); // -1 +1?
+    if (a.at(0) == -1 && b.at(0) == -1) {
+      return {-1, -1};
+    }
+    else if (a.at(0) == -1) {
+      long bx = b.at(0);
+      long by = b.at(1);
+      for (long i = lo; i < hi; i++) {
+	if (std::pow(bx-xCords.at(i), 2) + std::pow(by-yCords.at(i), 2) <= std::pow(d, 2)) {
+	  yCount++;
+	}
+      }
+      if (yCount > (hi - lo) / 2) {
+	return {bx, by};
+      }
+      else {
+	return {-1, -1};
+      }
+    }
+    else if (b.at(0) == -1) {
+      long ax = a.at(0);
+      long ay = a.at(1);
+      for (long i = lo; i < hi; i++) {
+	if (std::pow(ax-xCords.at(i), 2) + std::pow(ay-yCords.at(i), 2) <= std::pow(d, 2)) {
+	  xCount++;
+	}
+      }
+      if (xCount > (hi - lo) / 2) {
+	return {ax, ay};
+      }
+      else {
+	return {-1, -1};
+      }
+    }
+    else {
+      long bx = b.at(0);
+      long by = b.at(1);
+      long ax = a.at(0);
+      long ay = a.at(1);
+      for (long i = lo; i < hi; i++) {
+	if (std::pow(bx-xCords.at(i), 2) + std::pow(by-yCords.at(i), 2) <= std::pow(d, 2)) {
+	  yCount++;
+	}
+	if (std::pow(ax-xCords.at(i), 2) + std::pow(ay-yCords.at(i), 2) <= std::pow(d, 2)) {
+	  xCount++;
+	}
+      }
+      if (yCount > xCount && yCount > (hi - lo) / 2) {
+	return {bx, by};
+      }
+      else if (xCount > (hi - lo) / 2) {
+	return {ax, ay};
+      }
+      else {
+	return {-1, -1};
+      }
+    }
+  }
+}
 
-  return "";
+std::string findMajorGalaxy(std::vector<long> &xCords, std::vector<long> &yCords, long d, long k) {
+  std::vector<long> result = findMajority(xCords, yCords, 0, k, d);
+  if (result.at(0) != -1) {
+    long count = 0;
+    long x = result.at(0);
+    long y = result.at(0);
+    for (int i = 0; i < k; i++) {
+      if (std::pow(x-xCords.at(i), 2) + std::pow(y-yCords.at(i), 2) <= std::pow(d, 2)) {
+	count++;
+      }
+    }
+    return std::to_string(count);
+  }  
+  return "NO";
 }
 
 int main(int argc, char *argv[]) {
@@ -28,6 +111,6 @@ int main(int argc, char *argv[]) {
     yCords.push_back(val);
     kCount++;
   }
-
-  std::cout << findMajorGalaxy(xCords, yCords) << std::endl;
+  
+  std::cout << findMajorGalaxy(xCords, yCords, d, k) << std::endl;
 }
