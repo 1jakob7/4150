77a20234926c0f79b0aa779ba700e6546fb356a6
Jakob <u1092049@utah.edu>
2020-02-14 15:11:05 -0700

    failing 2nd-to-last test case

--- rumor.cpp
+++ rumor.cpp
@@ -1,9 +1,51 @@
+#include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <queue>
 #include <map>
 
+std::vector<std::string> generateReportBFS(std::vector<std::string> names, std::map<std::string, std::vector<std::string>> friends, std::string rumorStarter) {
+  std::map<std::string, int> dist;
+  std::map<std::string, std::string> prev;
+  for (std::string n : names) {
+    dist.insert(std::pair<std::string, int>(n, -1)); // -1 represents infinity
+    prev.insert(std::pair<std::string, std::string>(n, "")); // currently, no previous rumor teller
+  }
+  dist.at(rumorStarter) = 0; // the rumor starter
+  std::vector<std::string> report;
 
+  if (friends.count(rumorStarter) < 1) {
+    report.push_back(rumorStarter);
+  }
+  else {
+    std::queue<std::string> q;
+    q.push(rumorStarter);
+
+    while (!q.empty()) {
+      std::string curr = q.front();
+      q.pop();
+      for (std::string f : friends.at(curr)) {
+	if (dist.at(f) == -1) {
+	  q.push(f);
+	  dist.at(f) = dist.at(curr) + 1;
+	  prev.at(f) = curr;
+	}
+      }
+      report.push_back(curr);
+    }
+  }
+  
+  std::vector<std::string> nCopy = names;
+  std::sort(nCopy.begin(), nCopy.end());
+  for (std::string n : nCopy) {
+    if (dist.at(n) == -1) {
+      report.push_back(n);
+    }
+  }
+  
+  return report;
+}
 
 int main() {
   int n, f, r;
@@ -44,6 +86,12 @@ int main() {
       friends.at(friend2).push_back(friend1);
     }
   }
+  // get friend groups in alphabetical order
+  for (std::string s : names) {
+    if (friends.count(s) > 0) {
+      std::sort(friends.at(s).begin(), friends.at(s).end());
+    }
+  }
 
   std::getline(std::cin, data);
   std::stringstream ss3(data);
@@ -57,7 +105,16 @@ int main() {
     rumors.push_back(student);
   }
 
-  ///
+  for (unsigned int i = 0; i < rumors.size(); i++) {
+    std::vector<std::string> report = generateReportBFS(names, friends, rumors[i]);
+    for (unsigned int j = 0; j < report.size() - 1; j++) {
+      std::cout << report[j] << " ";
+    }
+    std::cout << report[report.size() - 1] << std::endl;
+  }
+  if (rumors.size() == 0) {
+    std::cout << std::endl;
+  }
   
   return 0;
 }
