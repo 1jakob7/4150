d28dbafa6192e2989dc4cfbcbfb0909e8ed67ce9
Jakob <jakob_horvath@hotmail.com>
2020-02-22 12:41:01 -0700

    passing sample tests

--- shorty.cpp
+++ shorty.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <sstream>
+#include <iomanip>
 #include <vector>
 #include <queue>
 #include <map>
 
 typedef std::pair<double, int> corr; // used for priority queue value pairing
 
+/// Uses a version of Dijkstra's algorithm to find the path of "least resistance". The starting value
+/// of '1.0' should be as high as possible after running thru the maze.
 double getHighestFrac(std::map<int, std::vector<double>> corridors, int numIntersects) {
   std::vector<double> dist(numIntersects);
   std::vector<int> prev(numIntersects);
   
   for (std::pair<int, std::vector<double>> ele : corridors) {
-    dist[ele.first] = 2.0; // because we're dealing with fractions, the path's value can't be > 1
+    dist[ele.first] = -1; ///
     prev[ele.first] = -1;
   }
   dist[0] = 1.0; // always start at intersection 0
 
   std::vector<bool> hasPopped(numIntersects); // used due to linear-time update operation for pq, and instead we include duplicates on the pq
-  std::priority_queue<corr, std::vector<corr>,std::greater<corr>> pq; // 'vector' and 'greater' ensure min queue
+  std::priority_queue<corr> pq;
   for (unsigned int i = 0; i < dist.size(); i++) {
     pq.push(std::make_pair(dist[i], i));
     hasPopped[i] = false;
   }
 
-  /// now, figure out how to handle intersections that have already been evaluated
   while (!pq.empty()) {
     corr curr = pq.top();
     pq.pop();
@@ -31,16 +33,14 @@ double getHighestFrac(std::map<int, std::vector<double>> corridors, int numInter
       hasPopped[curr.second] = true;
       std::vector<double> connections = corridors.at(curr.second);
       for (unsigned int i = 0; i < connections.size(); i+=2) { // new corridor every 2 elements
-        if (dist[connections[i]] > dist[curr.second] * connections[i+1]) { // comapring against fraction of current distance
+        if (dist[connections[i]] < dist[curr.second] * connections[i+1]) { // comparing against fraction of current distance
 	  dist[connections[i]] = dist[curr.second] * connections[i+1];
 	  prev[connections[i]] = curr.second;
 	  pq.push(std::make_pair(connections[i], dist[connections[i]]));
         }
       }
-      std::cout << curr.second << " " << curr.first << ", ";
     }
   }
-  std::cout << std::endl;
   return dist[numIntersects-1]; // the last intersection is always the exit
 }
 
@@ -78,6 +78,8 @@ int main() {
       }
     }
 
+    std::cout << std::fixed;
+    std::cout << std::setprecision(4);
     std::cout << getHighestFrac(corridors, n) << std::endl;
     
     std::getline(std::cin, data);
@@ -89,12 +91,3 @@ int main() {
   return 0;
 }
 
-    // std::map<int, std::vector<double>>::iterator it = corridors.begin();
-    // for (std::pair<int, std::vector<double>> element : corridors) {
-    //   std::cout << element.first << " ";
-    //   std::vector<double> v = element.second;
-    //   for (double d : v) {
-    // 	std::cout << d << " ";
-    //   }
-    //   std::cout << std::endl;
-    // }
