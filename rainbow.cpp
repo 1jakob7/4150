919959fdba864efa4ec00167648225dcb26d1919
Jakob <u1092049@utah.edu>
2020-03-29 12:51:51 -0600

    passed

--- rainbow.cpp
+++ rainbow.cpp
@@ -5,24 +5,31 @@
 #include <math.h>
 #include <algorithm>
 
-int dynamicMinPenalty(std::vector<int>& distances, int k) {
+int dynamicMinPenalty(std::vector<int>& distances, int k, int max, std::unordered_map<int, int>& cache) {
 
-  if (k == distances.size()-1) {
+  if (k == (int)distances.size()-1) {
     return 0;
   }
-  
-  int minC = pow(400 - (distances[distances.size()-1] - distances[0]), 2);
+
+  if (cache.count(k) > 0) {
+    return cache.at(k);
+  }
+
+  int minC = max;
   for (unsigned int j = k + 1; j < distances.size(); j++) {
     int cost = pow(400 - (distances[j] - distances[k]), 2);
-    minC = std::min(minC, cost + dynamicMinPenalty(distances, j));
+    minC = std::min(minC, cost + dynamicMinPenalty(distances, j, max, cache));
   }
-  
+
+  cache.insert(std::pair<int, int>(k, minC));
   return minC;
   
 }
 
 int dynamicMinPenalty(std::vector<int>& distances) {
-  return dynamicMinPenalty(distances, 0);
+  std::unordered_map<int, int> cache;
+  int max = pow(400 - (distances[distances.size()-1] - distances[0]), 2);
+  return dynamicMinPenalty(distances, 0, max, cache);
 }
 
 int main() {
