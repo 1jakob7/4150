222939f1dec42e319187961fcd5791719dd6f1bc
Jakob <u1092049@utah.edu>
2020-04-26 21:12:45 -0600

    greedy alg works, but too slow

--- fire.cpp
+++ fire.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <math.h>
 
 /// Greedy Alg.
+bool validSeparation(int minSeparation, int numShells, const std::vector<int>& distances) {
+  int shellCount = 1;
+  int combinedSeparation = 0;
+  for (int i = 0; i < (int)distances.size(); i++) {
+    combinedSeparation += distances[i];
+    if (combinedSeparation >= minSeparation) {
+      shellCount++;
+      combinedSeparation = 0;
+    }
+  }
+
+  return shellCount >= numShells;
+}
 
 /// Driver. Increase distance by 1 and try until it fails.
-int maxSeparation() {
-
-  return 0;
+int maxSeparation(int numShells, const std::vector<int>& distances) {
+  int currSeparation = 1;
+  while (validSeparation(currSeparation, numShells, distances)) {
+    currSeparation++;
+  }
+  
+  return currSeparation-1;
 }
 
 int main() {
@@ -18,13 +36,13 @@ int main() {
   ss >> n >> k;
 
   int d;
-  std::vector<int> dist;
+  std::vector<int> distances;
   for (int i = 0; i < n-1; i++) {
     std::getline(std::cin, data);
     ss.str(""); ss.clear();
     ss << data;
     ss >> d;
-    dist.push_back(d);
+    distances.push_back(d);
   }
 
   while (std::getline(std::cin, data)) {
@@ -32,7 +50,7 @@ int main() {
     ss.str(""); ss.clear();
     ss << data;
     ss >> d;
-    //
+    std::cout << maxSeparation(d, distances) << std::endl;
   }
 
   return 0;
