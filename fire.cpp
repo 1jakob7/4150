1cd9782c76e8e54da3ec3b68ec3b551cce13fa9c
Jakob <u1092049@utah.edu>
2020-04-27 22:03:58 -0600

    i like the interative version better

--- fire.cpp
+++ fire.cpp
@@ -3,11 +3,15 @@
 #include <vector>
 #include <math.h>
 
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <math.h>
+
 /// Greedy Alg.
-int maxSeparation(int minSeparation, int numShells, const std::vector<int>& distances) {
+bool validSeparation(int minSeparation, int numShells, const std::vector<int>& distances) {
   int shellCount = 1;
   int combinedSeparation = 0;
-  
   for (int i = 0; i < (int)distances.size(); i++) {
     combinedSeparation += distances[i];
     if (combinedSeparation >= minSeparation) {
@@ -16,10 +20,17 @@ int maxSeparation(int minSeparation, int numShells, const std::vector<int>& dist
     }
   }
 
-  if (shellCount >= numShells) {
-    return maxSeparation(minSeparation + 1, numShells, distances);
+  return shellCount >= numShells;
+}
+
+/// Driver. Increase distance by 1 and try until it fails.
+int maxSeparation(int numShells, const std::vector<int>& distances) {
+  int currSeparation = 1;
+  while (validSeparation(currSeparation, numShells, distances)) {
+    currSeparation++;
   }
-  return minSeparation - 1;
+  
+  return currSeparation-1;
 }
 
 int main() {
@@ -38,14 +49,14 @@ int main() {
     ss >> d;
     distances.push_back(d);
   }
-  
+
   while (std::getline(std::cin, data)) {
     if (data.empty()) break;
     ss.str(""); ss.clear();
     ss << data;
     ss >> d;
-    std::cout << maxSeparation(1, d, distances) << std::endl;
+    std::cout << maxSeparation(d, distances) << std::endl;
   }
-  
+
   return 0;
 }
